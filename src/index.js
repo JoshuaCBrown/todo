25b87aac11dde8fc73fe44575f489c702ba83327
JoshuaCBrown <[200~129564288+JoshuaCBrown@users.noreply.github.com~>
2023-06-22 22:03:57 -0400

    add more style add some function

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { showForm } from 'showform.js'
+
+const addTaskBtn = document.querySelector('.add-task');
+addTaskBtn.addEventListener('click', showForm)
---
+++ src/showform.js
@@ -0,0 +1,4 @@
+export const showForm = function() {
+    const myForm = document.querySelector('#add-form');
+    myForm.classList.add('show-this');
+};
